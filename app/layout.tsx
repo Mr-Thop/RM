d0ca59332487e20a05d98d58fd9afcd06f5a3afe
Mr-Thop <karthikreddy0314@gmail.com>
2025-08-04 23:36:21 +0530

    Add : Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'RM Agent',
+  description: 'Find Research Analysts Publishers etc...',
+  generator: 'Karthik Reddy',
 }
 
 export default function RootLayout({
